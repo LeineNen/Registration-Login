1648c35f5a5d97a4e3c76990a98c873f92e6a844
Mary Mag <maymag4on@gmail.com>
2024-09-17 13:55:10 +0000

    age

--- scripts/register.js
+++ scripts/register.js
@@ -1,10 +1,22 @@
 // Select form by id
-const registerForm = document.getElementById('register-form');
+const registerForm = document.querySelector('#register-form');
+console.log(registerForm);
 // Handle form submit event
-registerForm.onsubmit = function() {
+registerForm.addEventListener('submit', function () {
     // collect username
     // collect email
     // collect password
     // save user information
     // send confirmation email
+});
+
+// age variable
+const age = 44;
+// square of age
+function squareAge(age) {
+    const squaredAge = age ** 2;
+    return squaredAge;
 }
+
+const squaredAge = squareAge(45);
+console.log(squaredAge);
